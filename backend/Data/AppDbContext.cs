0ffd99b4eba4948bb45c26e8373ef8846494475e
felipehimself <felipecoutodarocha@gmail.com>
2023-05-29 20:18:59 -0300

    feat: added db context

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using InvoiceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvoiceApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options) { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceItem> InvoiceItems { get; set; }
+
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,6 @@
+using InvoiceApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// mysql connection
+string connectionString = builder.Configuration.GetConnectionString("MySqlConnection")!;
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33")));
 
 var app = builder.Build();
 
