7a8d0d26fc4d89514110c5bf547d544a5e84deff
felipehimself <felipecoutodarocha@gmail.com>
2023-05-29 21:09:30 -0300

    feat: added client repository and interface

---
+++ backend/Interfaces/IClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using InvoiceApp.Models;
+
+namespace InvoiceApp.Interfaces
+{
+    public interface IClientRepository
+    {
+        Task<Client?>? GetClient(int id);
+        Task<IEnumerable<Client>> GetClients();
+        Task<Client> PostClient(Client client);
+        Task PutClient(Client client);
+        Task DeleteClient(Client client);
+    }
+}
---
+++ backend/Repositories/ClientRepository.cs
@@ -0,0 +1,47 @@
+﻿using InvoiceApp.Data;
+using InvoiceApp.Interfaces;
+using InvoiceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvoiceApp.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ClientRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task DeleteClient(Client client)
+        {
+            _appDbContext.Remove(client);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<Client?>? GetClient(int id)
+        {
+            return await _appDbContext.Clients.AsNoTracking().SingleOrDefaultAsync(client => client.ClientId == id);
+
+        }
+
+        public async Task<IEnumerable<Client>> GetClients()
+        {
+            return await _appDbContext.Clients.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Client> PostClient(Client client)
+        {
+            await _appDbContext.Clients.AddAsync(client);
+            await _appDbContext.SaveChangesAsync();
+            return client;
+        }
+
+        public async Task PutClient(Client client)
+        {
+            _appDbContext.Clients.Update(client);
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
