30343ee260cc2a0414b3f3b7b588d4aa666cd007
felipehimself <felipecoutodarocha@gmail.com>
2023-05-29 23:21:56 -0300

    feat: added client dtos

---
+++ backend/DTOs/Client/ClientRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace InvoiceApp.DTOs
+{
+    public class ClientRequestDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ backend/DTOs/Client/ClientResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace InvoiceApp.DTOs
+{
+    public class ClientResponseDTO
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
