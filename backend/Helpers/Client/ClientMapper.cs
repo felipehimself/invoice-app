1a730792866346229fd8567bc1665a0d0004139c
felipehimself <felipecoutodarocha@gmail.com>
2023-05-29 23:29:36 -0300

    feat: added client mapper

---
+++ backend/Helpers/Client/ClientMapper.cs
@@ -0,0 +1,24 @@
+﻿using InvoiceApp.DTOs;
+using InvoiceApp.Models;
+
+namespace InvoiceApp.Helpers
+{
+    public class ClientMapper
+    {
+        public static ClientResponseDTO MapClientResponse(Client client)
+        {
+
+            return new ClientResponseDTO
+            {
+                ClientId = client.ClientId,
+                City = client.City,
+                Street = client.Street,
+                Country = client.Country,
+                Email = client.Email,
+                Name = client.Name,
+                PostalCode = client.PostalCode,
+
+            };
+        }
+    }
+}
