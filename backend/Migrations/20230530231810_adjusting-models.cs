a9b9a4f84e0fc7eb07d43bcac01a44b86e23d807
felipehimself <felipecoutodarocha@gmail.com>
2023-05-30 20:25:55 -0300

    build: created new migration

---
+++ backend/Migrations/20230530231810_adjusting-models.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class adjustingmodels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "InvoiceItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Invoices",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "Invoices",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<double>(
+                name: "Total",
+                table: "Invoices",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Total",
+                table: "InvoiceItems",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
