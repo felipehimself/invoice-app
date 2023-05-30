488b12e3683832dea948616c9b5dda23901c3f2d
felipehimself <felipecoutodarocha@gmail.com>
2023-05-30 20:40:25 -0300

    feat: added invoice repository interface

---
+++ backend/Interfaces/IInvoiceRepository.cs
@@ -0,0 +1,14 @@
+﻿using InvoiceApp.Models;
+
+namespace InvoiceApp.Interfaces
+{
+    public interface IInvoiceRepository
+    {
+        Task<IEnumerable<Invoice>> GetInvoices();
+        Task<Invoice> GetInvoice(int id);
+        Task<Invoice> PostInvoice(Invoice invoice);
+        Task PutInvoice(Invoice invoice);
+        Task DeleteInvoice(Invoice invoice);
+
+    }
+}
