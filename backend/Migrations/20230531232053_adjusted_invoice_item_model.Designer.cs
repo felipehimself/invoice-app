cf843420fc57b77bb57c586b823c3382d29581ba
felipehimself <felipecoutodarocha@gmail.com>
2023-05-31 20:21:29 -0300

    build: added new migration

--- backend/Migrations/20230531232053_adjusted_invoice_item_model.Designer.cs
+++ backend/Migrations/20230531232053_adjusted_invoice_item_model.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InvoiceApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InvoiceApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230531232053_adjusted_invoice_item_model")]
+    partial class adjusted_invoice_item_model
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Migrations/20230531232053_adjusted_invoice_item_model.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class adjusted_invoice_item_model : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "InvoiceItems",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "InvoiceItems");
+        }
+    }
+}
