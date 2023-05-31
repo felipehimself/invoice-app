5b3c3d7b952f8e9c0a6aec3da84644c83c66d47c
felipehimself <felipecoutodarocha@gmail.com>
2023-05-31 20:18:51 -0300

    feat: added dtos to handle post invoice

---
+++ backend/DTOs/Invoice/PostInvoiceDTO.cs
@@ -0,0 +1,28 @@
+﻿using InvoiceApp.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceApp.DTOs.Invoice
+{
+    public class PostInvoiceDTO
+    {
+
+        [Required]
+        [StringLength(100, MinimumLength = 5, ErrorMessage ="Description must be at leat 5 characters long")]
+        public string Description { get; set; }
+
+        [Required]
+        [StatusValidation]
+        public string Status { get; set; }
+
+        [Required]
+        public int DueInDays { get; set; }
+
+        [Required]
+        public int ClientId { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "At leat 1 item is required")]
+        public List<PostInvoiceItemDTO> Items { get; set; }
+
+    }
+}
---
+++ backend/DTOs/Invoice/PostInvoiceItemDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceApp.DTOs.Invoice
+{
+    public class PostInvoiceItemDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage ="At least 1 item is required")]
+        public int Quantity { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+    }
+}
