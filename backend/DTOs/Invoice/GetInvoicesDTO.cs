288aaa0f989c8aebc6fbbf29d9414e1cb52126df
felipehimself <felipecoutodarocha@gmail.com>
2023-05-31 20:03:06 -0300

    refactor: changed type to receive formatted string

--- backend/DTOs/Invoice/GetInvoicesDTO.cs
+++ backend/DTOs/Invoice/GetInvoicesDTO.cs
@@ -3,7 +3,7 @@
     public class GetInvoicesDTO
     {
         public int InvoiceId { get; set; }
-        public DateTime DueDate { get; set; }
+        public string DueDate { get; set; }
         public string ClientName { get; set; }
         public double Total { get; set; }
         public string Status { get; set; }
