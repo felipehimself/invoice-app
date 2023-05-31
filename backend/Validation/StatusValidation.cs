c9af3a2bc9c2aa705c27d694ac4b2a5f69dc347d
felipehimself <felipecoutodarocha@gmail.com>
2023-05-30 22:41:43 -0300

    feat: added custom validation class

---
+++ backend/Validation/StatusValidation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceApp.Validation
+{
+    public class StatusValidation : ValidationAttribute
+    {
+        private static readonly string[] _status = new[] { "pending", "paid", "draft" };
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            string valueToUpper = value!.ToString()!.ToUpper();
+            return (_status.Any(s => s.ToString().ToUpper().Equals(valueToUpper))) ?
+                ValidationResult.Success :
+                new ValidationResult("Accepted values: Pending, Paid or Draft");
+        }
+    }
+}
