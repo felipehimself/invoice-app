b3ba70e44ddc2f1578a8b82843641bfe9b9832fa
felipehimself <felipecoutodarocha@gmail.com>
2023-05-30 22:29:25 -0300

    buid: generated new migration considering the invoice property change

--- backend/Migrations/20230531012754_changed_property_name.Designer.cs
+++ backend/Migrations/20230531012754_changed_property_name.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InvoiceApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InvoiceApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230531012754_changed_property_name")]
+    partial class changed_property_name
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Migrations/20230531012754_changed_property_name.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class changed_property_name : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Paid",
+                table: "Invoices");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Invoices",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Invoices");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Paid",
+                table: "Invoices",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
