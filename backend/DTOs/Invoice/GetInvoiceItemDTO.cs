2f7def46e294dc0374fde4375e930b1f9deffc48
felipehimself <felipecoutodarocha@gmail.com>
2023-05-31 21:17:58 -0300

    feat: added new dto

---
+++ backend/DTOs/Invoice/GetInvoiceItemDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace InvoiceApp.DTOs.Invoice
+{
+    public class GetInvoiceItemDTO
+    {
+        public int InvoiceItemId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public double Total { get; set; }
+
+    }
+}
