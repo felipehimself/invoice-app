8592df754178eafba54b29b9bfc3ba39751ba55b
felipehimself <felipecoutodarocha@gmail.com>
2023-06-01 22:01:15 -0300

    feat: implemented generic repository interface

--- backend/Repositories/ClientRepository.cs
+++ backend/Repositories/ClientRepository.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace InvoiceApp.Repositories
 {
-    public class ClientRepository : IClientRepository
+    public class ClientRepository : IGenericRepository<Client>
     {
         private readonly AppDbContext _appDbContext;
 
@@ -14,31 +14,31 @@ namespace InvoiceApp.Repositories
             _appDbContext = appDbContext;
         }
 
-        public async Task DeleteClient(Client client)
+        public async Task Remove(Client client)
         {
             _appDbContext.Remove(client);
             await _appDbContext.SaveChangesAsync();
         }
 
-        public async Task<Client> GetClient(int id)
+        public async Task<Client?> FindById(int id)
         {
             return await _appDbContext.Clients.AsNoTracking().SingleOrDefaultAsync(client => client.ClientId == id);
 
         }
 
-        public async Task<IEnumerable<Client>> GetClients()
+        public async Task<IEnumerable<Client>> FindAll()
         {
             return await _appDbContext.Clients.AsNoTracking().ToListAsync();
         }
 
-        public async Task<Client> PostClient(Client client)
+        public async Task<Client> Add(Client client)
         {
             await _appDbContext.Clients.AddAsync(client);
             await _appDbContext.SaveChangesAsync();
             return client;
         }
 
-        public async Task PutClient(Client client)
+        public async Task Update(Client client)
         {
             _appDbContext.Clients.Update(client);
             await _appDbContext.SaveChangesAsync();
