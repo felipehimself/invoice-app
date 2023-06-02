b5461923c0195ea8037ad6f4391cc287068e4520
felipehimself <felipecoutodarocha@gmail.com>
2023-06-01 22:01:33 -0300

    feat: implemented generic repository interface

--- backend/Repositories/InvoiceRepository.cs
+++ backend/Repositories/InvoiceRepository.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace InvoiceApp.Repositories
 {
-    public class InvoiceRepository : IInvoiceRepository
+    public class InvoiceRepository : IGenericRepository<Invoice>
     {
         private readonly AppDbContext _appDbContext;
 
@@ -14,13 +14,13 @@ namespace InvoiceApp.Repositories
             _appDbContext = appDbContext;
         }
 
-        public async Task DeleteInvoice(Invoice invoice)
+        public async Task Remove(Invoice invoice)
         {
             _appDbContext.Invoices.Remove(invoice);
             await _appDbContext.SaveChangesAsync();
 
         }
-        public async Task<Invoice> GetInvoice(int id)
+        public async Task<Invoice?> FindById(int id)
         {
             return await _appDbContext.Invoices
                 .Include(inv => inv.Client)
@@ -29,21 +29,21 @@ namespace InvoiceApp.Repositories
                 .SingleOrDefaultAsync(inv => inv.InvoiceId == id);
         }
 
-        public async Task<IEnumerable<Invoice>> GetInvoices()
+        public async Task<IEnumerable<Invoice>> FindAll()
         {
             return await _appDbContext.Invoices.Include(inv => inv.Client).Include(inv => inv.InvoiceItems).AsNoTracking().ToListAsync();
         }
 
-        public async Task<Invoice> PostInvoice(Invoice invoice)
+        public async Task<Invoice> Add(Invoice invoice)
         {
             await _appDbContext.Invoices.AddAsync(invoice);
             await _appDbContext.SaveChangesAsync();
-            var invoiceResponse = await GetInvoice(invoice.InvoiceId);
-            return invoiceResponse;
+            var invoiceResponse = await FindById(invoice.InvoiceId);
+            return invoiceResponse!;
 
         }
 
-        public async Task PutInvoice(Invoice invoice)
+        public async Task Update(Invoice invoice)
         {
             _appDbContext.Invoices.Update(invoice);
             await _appDbContext.SaveChangesAsync();
