9ff1d0eacdd6c7ede14c07eeb12333466b4592d7
felipehimself <felipecoutodarocha@gmail.com>
2023-06-05 21:56:11 -0300

    feat: added custom validation

---
+++ backend/Validation/EmptyStringValidation.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceApp.Validation
+{
+    public class EmptyStringValidation : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            return string.IsNullOrEmpty(value.ToString().Trim()) ? new ValidationResult("Accepted values: Pending, Paid or Draft") : ValidationResult.Success;
+        }
+    }
+}
