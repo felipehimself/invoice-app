975d0790223336e23dde34461a4c3d80c9f3c596
felipehimself <felipecoutodarocha@gmail.com>
2023-06-05 21:31:46 -0300

    feat: added map type to show only date as the data format in frontend

--- backend/Program.cs
+++ backend/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => c.MapType<DateTime>(() => new Microsoft.OpenApi.Models.OpenApiSchema { Type = "string", Format = "date"}));
 
 // mysql connection
 string connectionString = builder.Configuration.GetConnectionString("MySqlConnection")!;
